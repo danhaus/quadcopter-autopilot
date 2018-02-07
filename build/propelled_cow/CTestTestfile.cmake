43f48c25677b5e441db23248275752f5e58fb805
Raspberry Pi <danhaus95@gmail.com>
2018-02-07 17:28:02 +0000

    creating ros envir and packages

---
+++ build/propelled_cow/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/propelled_cow/src/propelled_cow
+# Build directory: /home/pi/propelled_cow/build/propelled_cow
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

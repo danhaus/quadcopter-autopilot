e013e985dba7332fa170e7670141fd85318df14d
Raspberry Pi <danhaus95@gmail.com>
2018-02-14 15:12:40 +0000

    working example

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/pi/propelled_cow/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/pi/propelled_cow/devel;/home/pi/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/pi/propelled_cow/devel;/home/pi/catkin_ws/devel;/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/opt/ros/kinetic".split(';') if "/opt/ros/kinetic" != "" else []

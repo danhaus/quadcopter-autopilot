949ee385aab04ec05d6384df054fd76408318fe7
Raspberry Pi <danhaus95@gmail.com>
2018-03-12 11:57:51 +0000

    rosbag

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,5 +7,10 @@
 # new environment variables
 
 # modified environment variables
-export PATH="/opt/ros/kinetic/bin:/home/pi/bin:/home/pi/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export CMAKE_PREFIX_PATH="/home/pi/propelled_cow/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/pi/propelled_cow/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/pi/propelled_cow/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/pi/propelled_cow/build"
+export PYTHONPATH="/home/pi/propelled_cow/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/pi/propelled_cow/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/pi/propelled_cow/src:$ROS_PACKAGE_PATH"

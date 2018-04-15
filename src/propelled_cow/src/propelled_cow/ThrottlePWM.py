cb1c761b1c28a7f3f05de928bbbf41b3c9ffd38f
danhaus <danhaus95@gmail.com>
2018-04-15 22:16:27 +0100

    finished, needs to be tested

---
+++ src/propelled_cow/src/propelled_cow/ThrottlePWM.py
@@ -0,0 +1,25 @@
+import time
+import RPi.GPIO as GPIO
+
+class ThrottlePWM:
+
+	# constructor
+    def __init__(self, pin, freq=333, length=3000): # 333 Hz, 3000 us
+    	self.pin = pin
+    	self.pwm = GPIO.PWM(pin, freq) 
+        self.length = length # length of the whole cycle in us
+        self.start(0)
+
+    def start(self, val=0):
+        self.pwm.start(val)
+
+    def stop(self):
+	   self.pwm.stop()
+
+    def changeDutyCycle(self, dutyTime):
+        dutyCycle = dutyTime/self.length
+        self.pwm.ChangeDutyCycle(dutyCycle)
+
+if(__name__ == '__main__'): # connect servo to the pin below
+    pass
+

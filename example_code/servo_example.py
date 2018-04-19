c2acced6871093f0822d2a3e93d137c464994998
Raspberry Pi <danhaus95@gmail.com>
2018-04-19 12:48:25 +0100

    servo example working

--- example_code/servo_example.py
+++ example_code/servo_example.py
@@ -1,13 +1,14 @@
 import time
 import pigpio
 # FIRST RUN sudo pigpiod in terminal
-pin = 4
-pi = pigpio.pi() # Connect to local Pi.
+pin = 18
+pi = pigpio.pi() # Connect to local Pi
 pi.set_mode(pin, pigpio.OUTPUT)
-pi.set_servo_pulsewidth(17, 1000) # safe anti-clockwise
-time.sleep(1)
-pi.set_servo_pulsewidth(17, 1500) # centre
-time.sleep(1)
-pi.set_servo_pulsewidth(17, 2000) # safe clockwise
-time.sleep(1)
+pi.set_servo_pulsewidth(pin, 1000) # safe anti-clockwise
+time.sleep(2)
+pi.set_servo_pulsewidth(pin, 1500) # centre
+time.sleep(2)
+pi.set_servo_pulsewidth(pin, 2000) # safe clockwise
+time.sleep(2)
+pi.set_servo_pulsewidth(pin, 0) # swtich off
 pi.stop()

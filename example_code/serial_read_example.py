81627101aa8feee13b25ffee62f0ffe83ece4543
Raspberry Pi <danhaus95@gmail.com>
2018-05-05 16:43:02 +0000

    debugging

--- example_code/serial_read_example.py
+++ example_code/serial_read_example.py
@@ -7,11 +7,11 @@ last_received=''
 serbuffer = ''
 
 while 1:
-    time.sleep(5)
+    # time.sleep(5)
     serbuffer += ser.read(ser.inWaiting())
     if '\n' in serbuffer:
         lines = serbuffer.split('\n') # Guaranteed to have at least 2 entries
         last_received = lines[-2]
         serbuffer = lines[-1]
-    print('Waarde: ' + str(last_received))
-    time.sleep(25)
+    print('Reading [mm]: ' + str(last_received))
+    # time.sleep(25)

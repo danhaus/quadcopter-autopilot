f93353efa620719b4ca4580e28a6ee5409f38375
Raspberry Pi <danhaus95@gmail.com>
2018-05-06 18:06:40 +0000

    3rd flight manual

--- example_code/serial_simple_example.py
+++ example_code/serial_simple_example.py
@@ -2,13 +2,15 @@ import serial
 
 # ser = serial.Serial('/dev/ttyACM0', 9600)
 # ser = serial.Serial('/dev/ttyS0', 9600)
-ser = serial.Serial('/dev/ttyAMA0', 9600)
-# ser = serial.Serial('/dev/ttyUSB0', 9600)
+# ser = serial.Serial('/dev/ttyAMA0', 9600)
+ser = serial.Serial('/dev/ttyUSB0', 9600)
 
 
 
 while True:
 	try:
-		print int(ser.readline().strip())
+		# print int(ser.readline().strip())
+		print ser.readline()
+
 	except ValueError:
 		print "ValueError"
